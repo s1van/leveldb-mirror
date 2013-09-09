318cfd22222a37c3a3f7b5b696a1d6ebbd19ee13
siyuan <siyuan@pegasus-S5000XVN.(none)>
2013-09-09 11:22:07 -0400

    add threads in test.sh

--- misc/test.sh
+++ misc/test.sh
@@ -9,10 +9,15 @@ STORE=$4;
 DEVS=$5;
 
 USE_DB=0;
+THREADS=1;
 source $CONF; #test, benchmark, arguments
 
 prep_fillrandom1 () {
-	ARGS="--db=$STORE --benchmarks=$BENCHMARKS --num=$NUM --use_existing_db=$USE_DB";
+	ARGS="--db=$STORE --benchmarks=$BENCHMARKS --num=$NUM --use_existing_db=$USE_DB --threads=$THREADS";
+}
+
+prep_rwrandom1 () {
+	ARGS="--db=$STORE --benchmarks=$BENCHMARKS --num=$NUM --use_existing_db=$USE_DB --read_percent=$RRATIO --threads=$THREADS";
 }
 
 prep_$TEST;
